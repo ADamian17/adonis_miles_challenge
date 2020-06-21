62fc33afb0388cb8ecfe66eb2ecf58dfcbdb00f7
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2020-06-20 17:25:30 -0700

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import store from './redux/store';
-
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-toastify/dist/ReactToastify.css';
